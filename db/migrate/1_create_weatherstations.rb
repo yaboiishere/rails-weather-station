2740d575a9908c1d1a97b0899cb2f548ca90c626
Mihail Dobrev <mixaildobrev@gmail.com>
2021-02-14 17:18:55 +0200

    fix pls

--- db/migrate/1_create_weatherstations.rb
+++ db/migrate/1_create_weatherstations.rb
@@ -1,4 +1,4 @@
-class CreateWeatherStations < ActiveRecord::Migration[6.0]
+class CreateWeatherstations < ActiveRecord::Migration[6.0]
   def change
     create_table :weatherstations do |t|
       t.integer "weatherStation"
