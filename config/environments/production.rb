051c036643ce87ac3ae5ff1419006807cdea5a37
Mihail Dobrev <mixaildobrev@gmail.com>
2021-02-15 00:08:24 +0200

    typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,5 +103,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.web_socket_server_url = ENV("WS_URL")
+  config.web_socket_server_url = ENV["WS_URL"]
 end
